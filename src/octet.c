3d4d8d9bcabdee8ada0f53895e892c695fc252f1
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-12-02 13:07:29 +0100

    Suppression main() temporaire dans octet.h

--- src/octet.c
+++ src/octet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <math.h>
 
 #include "octet.h"
 
@@ -11,12 +10,4 @@ Bit O_obtenirIemeBit(Octet o, unsigned short i) {
     return (o >> (7-i)) & 1;
     //L'opérateur >> est un décalage de (7-i) vers la droite de l'octet o, ce qui correspond à une divison par 2^(7-i)
     //L'opérateur & compare bit à bit : en comparant avec 1 (0000001), on obtient la parité de l'opérande de gauche, ce qui correspond à un modulo 2
-}
-
-int main() {
-    Octet o = 234;
-    for (unsigned char i = 0; i <= 7; i++) {
-        printf("%d ",O_obtenirIemeBit(o,i));
-    }
-    printf("\n");
 }
