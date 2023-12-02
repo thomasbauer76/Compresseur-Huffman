b7cf6ce2caabc1542ddbf4ff547bd2b0a2294c1d
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-12-02 13:06:40 +0100

    Optimisation de la fonction obtenirIemeBit en utilisant des manipulations de bits

--- include/octet.h
+++ include/octet.h
@@ -9,7 +9,7 @@ typedef bool Bit;
 typedef unsigned char Octet;
 
 void O_creerOctet(Bit b0, Bit b1, Bit b2, Bit b3, Bit b4, Bit b5, Bit b6, Bit b7, Octet o);
-Bit O_obtenirIemeBit(Octet o, unsigned short b);
+Bit O_obtenirIemeBit(Octet o, unsigned short i);
 unsigned char O_octetVersNaturel(Octet o);
 Octet O_naturelVersOctet(unsigned char o);
 
--- src/octet.c
+++ src/octet.c
@@ -7,6 +7,16 @@ unsigned char O_octetVersNaturel(Octet o) {
     return o;
 }
 
-Bit O_obtenirIemeBit(Octet o, unsigned short b) {
-    return (o / (int)pow(2,7-b)) % 2;
+Bit O_obtenirIemeBit(Octet o, unsigned short i) {
+    return (o >> (7-i)) & 1;
+    //L'opérateur >> est un décalage de (7-i) vers la droite de l'octet o, ce qui correspond à une divison par 2^(7-i)
+    //L'opérateur & compare bit à bit : en comparant avec 1 (0000001), on obtient la parité de l'opérande de gauche, ce qui correspond à un modulo 2
+}
+
+int main() {
+    Octet o = 234;
+    for (unsigned char i = 0; i <= 7; i++) {
+        printf("%d ",O_obtenirIemeBit(o,i));
+    }
+    printf("\n");
 }
