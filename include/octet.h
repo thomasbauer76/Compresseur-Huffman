bb329880cb51fd9e9b03b8b8c109afe879becb08
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-12-05 09:53:45 +0100

    Changement nom variable dans naturelVersOctet (o -> n)

--- include/octet.h
+++ include/octet.h
@@ -11,7 +11,7 @@ typedef unsigned char Octet;
 void O_creerOctet(Bit b0, Bit b1, Bit b2, Bit b3, Bit b4, Bit b5, Bit b6, Bit b7, Octet o);
 Bit O_obtenirIemeBit(Octet o, unsigned short i);
 unsigned char O_octetVersNaturel(Octet o);
-Octet O_naturelVersOctet(unsigned char o);
+Octet O_naturelVersOctet(unsigned char n);
 
 
 #endif
