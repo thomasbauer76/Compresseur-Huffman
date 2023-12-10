a1a0c7d644a6bf63bf7e6ac065436ca234e45e05
Mister-S <ninjago78@orange.fr>
2023-12-10 23:15:32 +0100

    création de arbreDeHuffman.c (fonctions vides)

---
+++ src/arbreDeHuffman.c
@@ -0,0 +1,32 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "arbreDeHuffman.h"
+
+ArbreDeHuffman ADH_arbreDeHuffman(Octet o, unsigned long n) {
+    return NULL;
+}
+
+ArbreDeHuffman ADH_fusionner(ArbreDeHuffman ag, ArbreDeHuffman ad) {
+    return NULL;
+}
+
+bool ADH_estUneFeuille(ArbreDeHuffman a) {
+    return 0;
+}
+
+//Octet est un tableau, c'est donc une Entrée/Sortie
+void ADH_obtenirOctet(ArbreDeHuffman a, Octet o) {
+
+}
+
+unsigned long ADH_obtenirFrequence(ArbreDeHuffman a) {
+    return 0;
+}
+
+ArbreDeHuffman ADH_obtenirFilsGauche(ArbreDeHuffman a) {
+    return NULL;
+}
+
+ArbreDeHuffman ADH_obtenirFilsDroit(ArbreDeHuffman a) {
+    return NULL;
+}
