cf92148281c2adbfe60596758731469af550b4f0
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-12-12 12:54:02 +0100

    conversion de creerOctet de procédure à fonction

--- src/octet.c
+++ src/octet.c
@@ -2,8 +2,8 @@
 
 #include "octet.h"
 
-void O_creerOctet(Bit b0, Bit b1, Bit b2, Bit b3, Bit b4, Bit b5, Bit b6, Bit b7, Octet o){
-	o=b7+(b6*2)+(b5*2^2)+(b4*2^3)+(b3*2^4)+(b2*2^5)+(b1*2^6)+(b0*2^7);
+Octet O_creerOctet(Bit b0, Bit b1, Bit b2, Bit b3, Bit b4, Bit b5, Bit b6, Bit b7){
+	return b7+(b6*2)+(b5*2^2)+(b4*2^3)+(b3*2^4)+(b2*2^5)+(b1*2^6)+(b0*2^7);
 }	
 Bit O_obtenirIemeBit(Octet o, unsigned short i) {
     return (o >> (7-i)) & 1;
@@ -18,4 +18,3 @@ unsigned char O_octetVersNaturel(Octet o) {
 Octet O_naturelVersOctet (unsigned char n){
 	return n;
 }
-
