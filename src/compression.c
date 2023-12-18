45e5713c9babce29a398f3a004ff7b4df060d790
Ouathrani Taoba <taoba.ouathrani@insa-rouen.fr>
2023-12-18 23:55:04 +0100

    construirefileDeprio

--- src/compression.c
+++ src/compression.c
@@ -23,7 +23,22 @@ void obtenirStatistiquesEtTailleFichier(FILE *f, Statistiques *s,  unsigned long
 }
 
 FileDePriorite construireFileDePriorite(Statistiques s) {
-    return NULL;
+    FileDePriorite fdp;
+    Octet octet;
+    unsigned long occurence;
+
+    fdp = FDPAH_fileDePriorite();
+
+    for(unsigned char o = 0; o < 256; o++) {
+        octet = O_naturelVersOctet(o);
+        occurence = S_obtenirOccurence(s, octet);
+
+        if(occurence > 0) {
+            FDPAH_enfiler(&fdp, ADH_arbreDeHuffman(octet, occurence));
+        }
+    }
+
+    return fdp;
 }
 
 ArbreDeHuffman construireArbreDeHuffman(Statistiques s){
