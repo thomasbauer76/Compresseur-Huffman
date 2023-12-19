f3563335fac5039b8b030e0f1a364bcd50cb72f4
Thomas Bauer <thomas.bauer@insa-rouen.fr>
2023-12-19 10:21:39 +0100

    Creation construireArbreDeHuffman.h

---
+++ include/construireArbreDeHuffman.h
@@ -0,0 +1,13 @@
+#ifndef __CONSTRUIRE_ARBRE_DE_HUFFMAN__
+#define __CONSTRUIRE_ARBRE_DE_HUFFMAN__
+
+
+#include "statistiques.h"
+#include "arbreDeHuffman.h"
+#include "fileDePrioriteDArbreDeHuffman.h"
+
+FileDePriorite CADH_construireFileDePriorite(Statistiques s);
+ArbreDeHuffman CADH_construireArbreDeHuffman(Statistiques s);
+
+
+#endif
