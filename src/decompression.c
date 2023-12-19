353d7d935eed6136d05fdc0bed3399a31ed9dfe8
Mister-S <ninjago78@orange.fr>
2023-12-19 07:41:42 +0100

    .h et fonctions vides de seDeplacerDansLArbre et libererADH

--- src/decompression.c
+++ src/decompression.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include "decompression.h"
 #include "fileDePrioriteDArbreDeHuffman.h"
 #include "statistiques.h"
 
 void decompresser(FILE *f) {
 
+}
+
+void seDeplacerDansLArbre(int bit, ArbreDeHuffman *a) {
+
+}
+
+void libererArbreDeHuffman(ArbreDeHuffman a) {
+    
 }
