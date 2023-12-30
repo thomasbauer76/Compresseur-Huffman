3b11c2295a309999975419a18d80c392ae7e46f7
Mister-S <ninjago78@orange.fr>
2023-12-31 00:40:02 +0100

    Début  de la doc sur ADH (auteur et types)

--- include/arbreDeHuffman.h
+++ include/arbreDeHuffman.h
@@ -1,3 +1,11 @@
+/**
+ * \file arbreDeHuffman.h
+ * \brief Implantation du TAD ArbreDeHuffman pour le compresseur d'Huffman
+ * \author M. Saunier
+ * \date 31/12/2023
+ *
+ */
+
 #ifndef __ARBRE_DE_HUFFMAN__
 #define __ARBRE_DE_HUFFMAN__
 
@@ -5,8 +13,19 @@
 #include <stdbool.h>
 #include "octet.h"
 
+/**
+ * \brief Le type ArbreDeHuffman est un pointeur vers un NoeudArbreDeHuffman
+ * 
+ */
 typedef struct NoeudArbreDeHuffman *ArbreDeHuffman;
 
+/**
+ * \brief Le type NoeudArbreDeHuffman est une structure qui contient un octet ainsi qu'une fréquence
+ * associée à cet octet. Ces valeurs n'ont un sens que si le noeud est une feuille et cette information
+ * est stocée dans le booléen 'estUneFeuille'. Les deux derniers champs de cette structure sont 2 ArbreDeHuffman
+ * qui représentent un fils gauche et un fils droit
+ * 
+ */
 typedef struct NoeudArbreDeHuffman {
     Octet octet;
     unsigned long frequence;
