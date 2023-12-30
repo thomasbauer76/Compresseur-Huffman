44bd6650ddd47973d316996a36b07f2c3ea41f85
Mister-S <ninjago78@orange.fr>
2023-12-31 00:46:32 +0100

    Doc de ADH 4 fonctions sur 8

--- include/arbreDeHuffman.h
+++ include/arbreDeHuffman.h
@@ -21,7 +21,7 @@ typedef struct NoeudArbreDeHuffman *ArbreDeHuffman;
 
 /**
  * \brief Le type NoeudArbreDeHuffman est une structure qui contient un octet ainsi qu'une fréquence
- * associée à cet octet. Ces valeurs n'ont un sens que si le noeud est une feuille et cette information
+ * associée à cet octet. La valeur de l'octet n'a de sens que si le noeud est une feuille et cette information
  * est stocée dans le booléen 'estUneFeuille'. Les deux derniers champs de cette structure sont 2 ArbreDeHuffman
  * qui représentent un fils gauche et un fils droit
  * 
@@ -34,9 +34,43 @@ typedef struct NoeudArbreDeHuffman {
     ArbreDeHuffman arbreDroit;
 } NoeudArbreDeHuffman;
 
+/**
+ * \fn ArbreDeHuffman ADH_arbreDeHuffman(Octet o, unsigned long n)
+ * \brief Fonction de création d'un ArbreDeHuffman "feuille" (avec un octet et une fréquence associée)
+ *
+ * \param o : l'octet
+ * \param n : la fréquence associé à l'octet
+ * \return ArbreDeHuffman
+ */
 ArbreDeHuffman ADH_arbreDeHuffman(Octet o, unsigned long n);
+
+/**
+ * \fn ArbreDeHuffman ADH_fusionner(ArbreDeHuffman ag, ArbreDeHuffman ad)
+ * \brief Fonction permettant la fusion de 2 ArbreDeHuffman avec une racine qui a pour fréquence la somme des fréquences des 2 ArbreDeHuffman
+ *
+ * \param ag : ArbreDeHuffman
+ * \param ad : ArbreDeHuffman
+ * \return ArbreDeHuffman
+ */
 ArbreDeHuffman ADH_fusionner(ArbreDeHuffman ag, ArbreDeHuffman ad);
+
+/**
+ * \fn ADH_estUneFeuille(ArbreDeHuffman a)
+ * \brief Fonction permettant de savoir si un ArbreDeHuffman est une feuille
+ *
+ * \param a : ArbreDeHuffman
+ * \return Booleen
+ */
 bool ADH_estUneFeuille(ArbreDeHuffman a);
+
+/**
+ * \fn ADH_obtenirOctet(ArbreDeHuffman a)
+ * \brief Fonction permettant d'obtenir l'octet d'un ArbreDeHuffman
+ *
+ * \param a : ArbreDeHuffman
+ * \return Octet
+ */
+
 Octet ADH_obtenirOctet(ArbreDeHuffman a);
 unsigned long ADH_obtenirFrequence(ArbreDeHuffman a);
 ArbreDeHuffman ADH_obtenirFilsGauche(ArbreDeHuffman a);
