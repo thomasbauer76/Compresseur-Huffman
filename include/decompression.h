5e87ef1a0a23e6e3897a6daad7e7167a470ae9ac
Mister-S <ninjago78@orange.fr>
2023-12-31 11:53:42 +0100

    Documentation de decompression.h

--- include/decompression.h
+++ include/decompression.h
@@ -1,9 +1,24 @@
+/**
+ * \file decompression.h
+ * \brief Implantation de la fonction decompresser pour le compresseur d'Huffman
+ * \author M. Saunier
+ * \date 31/12/2023
+ *
+ */
+
 #ifndef __DECOMPRESSION__
 #define __DECOMPRESSION__
 
 
 #include <stdio.h>
 
+/**
+ * \fn D_decompresser(FILE *fbCompresse, char *filename)
+ * \brief Fonction permettant la décompression d'un fichier, dont le nom est donné en entrée, préalablement compressé par notre algortithme du compresseur d'Huffman. Le fichier décompressé est crée par la fonction et portera le nom du fichier compressé sans l'extension '.huff'
+ *
+ * \param fbCompresse : le FichierBinaire compressé
+ * \param filename : le nom du fichier compressé
+ */
 void D_decompresser(FILE *fbCompresse, char *filename);
 
 
