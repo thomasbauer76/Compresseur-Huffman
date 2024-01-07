ea17145857982382e07cac3cab8c457805dbbbd9
Ouathrani Taoba <taoba.ouathrani@insa-rouen.fr>
2024-01-07 03:00:50 +0100

    docs

--- include/compression.h
+++ include/compression.h
@@ -1,3 +1,10 @@
+/**
+ * \file compression.h
+ * \brief Implémentation de la fonction compresser pour le compresseur d'Huffman
+ * \author O.Taoba
+ * \date 07/01/2024
+ *
+ */
 #ifndef __COMPRESSION__
 #define __COMPRESSION__
 
@@ -5,7 +12,13 @@
 #include <stdio.h>
 
 #define IDENTIFIANT 1000
-
+/**
+ * \fn C_Compresser(FILE *f, char *filename)
+ * \brief Fonction permettant la compression d'un fichier, dont le nom est donné en entrée afin de reduire le nombre d'octet à la décompression en gardant exactement les mêmes données. 
+ *
+ * \param f : le FichierBinaire initiale
+ * \param filename : le nom du FichierBinaire compressé
+ */
 void C_compresser(FILE *f, char *filename);
 
 #endif
--- include/tableDeCodage.h
+++ include/tableDeCodage.h
@@ -1,19 +1,60 @@
+/**
+ * \file statistiques.h
+ * \brief Implémentation du TAD TableDeCodage pour le compresseur d'Huffman
+ * \author O.Taoba
+ * \date 07/01/2024
+ *
+ */
 #ifndef __TABLE_DE_CODAGE__
 #define __TABLE_DE_CODAGE__
+
 
 #include <stdbool.h>
 
 #include "codeBinaire.h"
 #include "octet.h"
-
+/**
+ * \brief Le type TDC_TableDeCodage est une structure contenant qui contient 2 champs : un tableau de CodeBinaire et un Tableau de Booleen qui retourne le booleen associé à la présence d'un code binaire associé à un octet particulier
+ *
+ */
 typedef struct TDC_TableDeCodage {
     CB_CodeBinaire tableDeCodeBinaire[MAX_OCTET];
     bool tableDePresence[MAX_OCTET];
 } TDC_TableDeCodage;
 
+/**
+ * \fn TDC_TableDeCodage TDC_creerTableCodage(void)
+ * \brief Fonction créant une TableDeCodage Vide
+ *
+ * \return TDC_TableDeCodage
+ */
 TDC_TableDeCodage TDC_creerTableCodage();
+
+/**
+ * \fn void TDC_ajouterCodage(TDC_TableDeCodage* p_tdc, O_Octet o, CB_CodeBinaire cb)
+ * \brief Fonction permettant ajouter un codeBinaire à notre table de codage
+ *
+ * \param p_tdc : un pointeur sur la table de codage à modifié
+ * \param o : l'octet en entree correspondant à l'octet associé au code binaire qui va etre inserer
+ * \param cb : le codeBinaire à ajouter à la tableDeCodage
+ */
 void TDC_ajouterCodage(TDC_TableDeCodage* p_tdc, O_Octet o, CB_CodeBinaire cb);
+/**
+ * \fn CB_CodeBinaire TDC_octetVersCodeBinaire(TDC_TableDeCodage tdc, O_Octet o)
+ * \brief Procédure permettant de retourner le codeBinaire associé à un octet en entrée à partir de la tableDeCodage
+ *
+ * \param tdc : la table de codage à partir de laquelle je recupère mes données
+ * \param o : l'octet dont on veut recuper le code binaire
+ * \return CB_CodeBinaire
+ */
 CB_CodeBinaire TDC_octetVersCodeBinaire(TDC_TableDeCodage tdc, O_Octet o);
+/**
+ * \fn bool TDC_octetPresent(TDC_TableDeCodage tdc, O_Octet o)
+ * \brief Fonction qui retourne un booleen afin de savoir si un octet est present dans notre table de codage
+ * \param tdc : la table de codage à partir de laquelle je recupère mes données
+ * \param o : l'octet dont on veux verifier la présence
+ * \return Booleen
+ */
 bool TDC_octetPresent(TDC_TableDeCodage tdc, O_Octet o);
 
 #endif
